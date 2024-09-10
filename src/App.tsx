972fddc14c88a77c846c171785d6901223d2b0dc
amirmh66 <looperxz67@gmail.com>
2024-09-10 21:10:09 +0330

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AdminLayout from './Component/Layouts/AdminLayout'
+import './Component/GlobalStyle/Glogbal.css'
+
+function App() {
+  return (
+    <>
+      <AdminLayout />
+      
+    </>
+  );
+}
+
+export default App
---
+++ src/Component/Pages/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Leaderboard() {
+    return (
+        <>
+            <div className="card-bg-white m-5">
+                <p>Im Chart.js</p>
+                <canvas id="myChart"></canvas>
+            </div>
+        </>
+    )
+}
