600e1eeecaccac4a99ef5779cc98b38b5fadcc84
amirmh66 <looperxz67@gmail.com>
2024-12-07 20:28:31 +0330

    style : update styles for table skeleton to support dark mode

--- Frontend/src/Component/Elements/TablesSkeleton.tsx
+++ Frontend/src/Component/Elements/TablesSkeleton.tsx
@@ -1,126 +1,112 @@
+import { FaDeviantart } from "react-icons/fa"
+
 function TablesSkeleton() {
 
     return (
         <>
+            {/* table-nav */}
             <div className="drop-shadow mb-2 p-3 bg-white dark:bg-gray-900 rounded-lg flex gap-5 items-center">
                 <div className='animate-pulse flex gap-5'>
-                    <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default  h-12'></button>
-                    <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default  h-12'></button>
-                    <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default  h-12'></button>
+                    <button className='btn-skeleton'></button>
+                    <button className='btn-skeleton'></button>
+                    <button className='btn-skeleton'></button>
                 </div>
             </div>
 
-            <div className="shadow-md bg-gray-200 w-full rounded-lg  dark:bg-gray-700">
+            <div className="shadow-md bg-gray-200 w-full dark:bg-gray-700 rounded-lg ">
 
                 {/* header */}
                 <div className='flex justify-start gap-48 px-5 items-center h-16' >
                     <div>
-                        <div className='bg-gray-100 rounded-lg w-32 h-6 animate-pulse'></div>
+                        <div className='txt-header-skeleton'></div>
                     </div>
                     <div>
-                        <div className='bg-gray-100 rounded-lg w-32 h-6 animate-pulse'></div>
+                        <div className='txt-header-skeleton'></div>
                     </div>
                     <div>
-                        <div className='bg-gray-100 rounded-lg w-32 h-6 animate-pulse'></div>
+                        <div className='txt-header-skeleton'></div>
                     </div>
                 </div>
 
                 {/* body */}
-                <div className="bg-gray-50 900 h-20 pl-10  flex gap-44 justify-start items-center">
-
+                <div className="body-table">
+                    {/* nameOnTr   */}
                     <div className="py-4 px-5">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-4 px-5 ">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                    <div className="py-4 px-5">
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-2 px-5 ">
+                    {/* buttons */}
+                    <div className="py-2 px-5">
                         <div className='flex gap-5'>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default h-12 animate-pulse'></button>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default mr-4 h-12 animate-pulse'></button>
+                            <div className='btn-skeleton'></div>
+                            <div className='btn-skeleton'></div>
                         </div>
                     </div>
-
-
                 </div>
-                <div className="bg-gray-50 900 h-20 pl-10  flex gap-44 justify-start items-center">
-
+                <div className="body-table">
+                    {/* nameOnTr   */}
                     <div className="py-4 px-5">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-4 px-5 ">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                    <div className="py-4 px-5">
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
+                    {/* buttons */}
                     <div className="py-2 px-5 ">
                         <div className='flex gap-5'>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default h-12 animate-pulse'></button>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default mr-4 h-12 animate-pulse'></button>
+                            <div className='btn-skeleton'></div>
+                            <div className='btn-skeleton'></div>
                         </div>
                     </div>
-
-
-                </div>
-                <div className="bg-gray-50 900 h-20 pl-10  flex gap-44 justify-start items-center">
-
+                </div><div className="body-table">
+                    {/* nameOnTr   */}
                     <div className="py-4 px-5">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-4 px-5 ">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                    <div className="py-4 px-5">
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
+                    {/* buttons */}
                     <div className="py-2 px-5 ">
                         <div className='flex gap-5'>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default h-12 animate-pulse'></button>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default mr-4 h-12 animate-pulse'></button>
+                            <div className='btn-skeleton'></div>
+                            <div className='btn-skeleton'></div>
                         </div>
                     </div>
-
-
-                </div>
-                <div className="bg-gray-50 900 h-20 pl-10  flex gap-44 justify-start items-center">
-
+                </div><div className="body-table">
+                    {/* nameOnTr   */}
                     <div className="py-4 px-5">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-4 px-5 ">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                    <div className="py-4 px-5">
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
+                    {/* buttons */}
                     <div className="py-2 px-5 ">
                         <div className='flex gap-5'>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default h-12 animate-pulse'></button>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default mr-4 h-12 animate-pulse'></button>
+                            <div className='btn-skeleton'></div>
+                            <div className='btn-skeleton'></div>
                         </div>
                     </div>
-
-
-                </div>
-                <div className="bg-gray-50 900 h-20 pl-10  flex gap-44 justify-start items-center">
-
+                </div><div className="body-table">
+                    {/* nameOnTr   */}
                     <div className="py-4 px-5">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
-                    <div className="py-4 px-5 ">
-                        <div className='bg-gray-200 w-28 h-8 rounded-lg animate-pulse'></div>
+                    <div className="py-4 px-5">
+                        <div className='txt-name-On-Tr'></div>
                     </div>
-
+                    {/* buttons */}
                     <div className="py-2 px-5 ">
                         <div className='flex gap-5'>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default h-12 animate-pulse'></button>
-                            <button className='bg-gray-200 rounded-lg p-2 w-28 cursor-default mr-4 h-12 animate-pulse'></button>
+                            <div className='btn-skeleton'></div>
+                            <div className='btn-skeleton'></div>
                         </div>
                     </div>
-
-
                 </div>
+
             </div>
         </>
     )
