35677653688cccf9ce1511eb637710af5df834e1
amirmh66 <looperxz67@gmail.com>
2025-02-10 14:31:50 +0330

    feat: Develop and complete notification UI in admin header panel

--- Frontend/src/Component/PanelAdmin/Header/navbar/Notification.tsx
+++ Frontend/src/Component/PanelAdmin/Header/navbar/Notification.tsx
@@ -1,28 +1,12 @@
-import React from 'react'
+import { BellIcon } from "@heroicons/react/24/outline"
 
 function Notification() {
   return (
     <>
-       <div className="bg-yellow-200 p-2 rounded-lg mx-5 hidden md:block ">
-            <a href="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="size-6 text-yellow-500"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                />
-              </svg>
-            </a>
-
-            <span className="bg-red-600"></span>
-          </div>
+      <div className="bg-yellow-100 p-2 rounded-lg mx-5 hidden md:block relative group cursor-pointer ">
+        <div className="absolute bg-red-600 rounded-full w-2 h-2 inline-block top-1 right-1"></div>
+        <BellIcon className='w-7 text-yellow-500 group-hover:rotate-12 transition-all duration-150 ease-linear' />
+      </div>
     </>
   )
 }
