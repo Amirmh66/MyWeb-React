b8730d9923b947f19c7d964ff74815ad2a5472ab
amirmh66 <looperxz67@gmail.com>
2025-05-12 23:00:25 +0330

    feat(homepage/mobile): add DynamicProductSlider for featured products - Create DynamicProductSlider component - Implement dynamic product list from API

--- Frontend/src/Component/OrgSite/pages/HomePage/Mobile/Mobile.tsx
+++ Frontend/src/Component/OrgSite/pages/HomePage/Mobile/Mobile.tsx
@@ -5,7 +5,24 @@ import NavbarMobile from './Sections/NavbarMobile'
 import Logo from '../../../../Elements/Logo'
 import SearchBox from '../Shared/SearchBox'
 import BrandSpotlight from './Sections/BrandSpotlight'
+import DynamicProductSlider from './Sections/DynamicProductSlider'
 import "./Mobile.css";
+
+type ProductType = {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+}
+
+const cardItem: ProductType[] = [
+    { id: 1, name: "Xiaomi Redmi Note 13 4G 256GB Storage 8GB ", price: 100, imageUrl: "mobile.jpg" },
+    { id: 2, name: "Xiaomi Redmi Note 13 4G 256GB Storage 8GB RAM", price: 80, imageUrl: "mobile.jpg" },
+    { id: 3, name: "Xiaomi Redmi Note 13 4G 256GB Storage", price: 10, imageUrl: "mobile.jpg" },
+    { id: 4, name: "Xiaomi Redmi Note 13 4G 256GB Storage 8GB RAM", price: 1200, imageUrl: "mobile.jpg" },
+    { id: 5, name: "Xiaomi Redmi Note 13 4G 256GB ", price: 1299, imageUrl: "mobile.jpg" },
+    { id: 6, name: "Xiaomi Redmi Note 13 4G 256GB Storage 8GB RA", price: 599, imageUrl: "mobile.jpg" },
+]
 
 function Mobile() {
     return (
@@ -17,6 +34,9 @@ function Mobile() {
                 <WebBanners />
                 <ShoppingByCategories />
                 <BrandSpotlight />
+                <DynamicProductSlider productsDetail={cardItem} title='Best-SellingProducts' />
+                <DynamicProductSlider productsDetail={cardItem} title='Featured-Products' />
+                <DynamicProductSlider productsDetail={cardItem} title='DiscountedProducts' />
                 <nav>
                     <NavbarMobile />
                 </nav>
---
+++ Frontend/src/Component/OrgSite/pages/HomePage/Mobile/Sections/DynamicProductSlider.tsx
@@ -0,0 +1,50 @@
+import { ShareIcon, HeartIcon } from "@heroicons/react/20/solid"
+// import { HeartIcon } from "@heroicons/react/24/outline"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+type ProductType = {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+}
+
+const DynamicProductSlider = ({ productsDetail, title }: { productsDetail: ProductType[]; title: string }) => {
+    const productDetails = productsDetail;
+    return (
+        <>
+            <div className="flex flex-col gap-3">
+                <div className="flex justify-between">
+                    <p className="font-bold">{title}</p>
+                    <p className="text-blue-500 font-bold hover:text-blue-700">Show all</p>
+                </div>
+                <div>
+                    <Swiper
+                        spaceBetween={10}
+                        slidesPerView={2.1}
+                        freeMode={true}>
+                        {productDetails.map((i) => (
+                            <SwiperSlide key={i.id}>
+                                <div className="relative flex flex-col gap-1 p-1 rounded-lg bg-white h-80 overflow-hidden">
+                                    <img className="object-cover" srcSet={`Images/${i.imageUrl}`} alt={i.name} />
+                                    <div className="absolute top-[55%] left-1 text-left flex flex-col justify-between">
+                                        <p className="font-semibold px-2 text-sm line-clamp-3">{i.name}</p>
+                                    </div>
+                                    <div className="absolute bottom-1 left-1 flex flex-col gap-2">
+                                        <p className="font-bold">$ {i.price.toLocaleString()}</p>
+                                        <div className="flex gap-2 ">
+                                            <HeartIcon className="w-6  text-gray-500 border-black  hover:text-red-500 " />
+                                            <ShareIcon className="w-6" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DynamicProductSlider
